9a926fcc79b80f7c08aea57465e616a0ae65887a
jboehler <johann.boehler@1und1.de>
2015-02-27 14:05:27 +0100

    added test

--- src/test/java/com/oneandone/network/snmpman/configuration/modifier/AbstractIntegerModifierTest.java
+++ src/test/java/com/oneandone/network/snmpman/configuration/modifier/AbstractIntegerModifierTest.java
@@ -3,6 +3,8 @@ package com.oneandone.network.snmpman.configuration.modifier;
 import com.oneandone.network.snmpman.configuration.type.ModifierProperties;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
+
 public class AbstractIntegerModifierTest {
 
     @Test(expectedExceptions = { IllegalArgumentException.class })
@@ -27,6 +29,23 @@ public class AbstractIntegerModifierTest {
 
         final Gauge32Modifier modifier = new Gauge32Modifier();
         modifier.init(modifierProperties);
+    }
+
+    @Test
+    public void testProperties() throws Exception {
+        final ModifierProperties modifierProperties = new ModifierProperties();
+        modifierProperties.put("minimum", 0);
+        modifierProperties.put("maximum", 10);
+        modifierProperties.put("minimumStep", 1);
+        modifierProperties.put("maximumStep", 10);
+
+        final Gauge32Modifier modifier = new Gauge32Modifier();
+        modifier.init(modifierProperties);
+
+        assertEquals(modifier.getMinimum(), new Long(0));
+        assertEquals(modifier.getMaximum(), new Long(10));
+        assertEquals(modifier.getMinimumStep(), new Long(1));
+        assertEquals(modifier.getMaximumStep(), new Long(10));
     }
     
 }
