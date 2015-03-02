65dfd8bb7aca20f15c192995121950310dcb524a
jboehler <johann.boehler@1und1.de>
2015-03-02 18:10:42 +0100

    fixed bulk test

--- src/test/java/com/oneandone/network/snmpman/SnmpmanTest.java
+++ src/test/java/com/oneandone/network/snmpman/SnmpmanTest.java
@@ -1,15 +1,19 @@
 package com.oneandone.network.snmpman;
 
 import org.snmp4j.*;
-import org.snmp4j.event.ResponseEvent;
 import org.snmp4j.mp.SnmpConstants;
 import org.snmp4j.smi.*;
 import org.snmp4j.transport.DefaultUdpTransportMapping;
+import org.snmp4j.util.DefaultPDUFactory;
+import org.snmp4j.util.PDUFactory;
+import org.snmp4j.util.TableEvent;
+import org.snmp4j.util.TableUtils;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.util.List;
 
 import static org.testng.Assert.assertEquals;
 
@@ -24,22 +28,22 @@ public class SnmpmanTest {
 
     @Test
     public void testSnmpGetBulk() throws Exception {
-        PDU responsePDU = getResponse(new OID("1.3.6.1.2.1"));
-        assertEquals(responsePDU.getVariableBindings().size(), 19); // 18 + endOfMibView
+        List<TableEvent> responses = getResponse(new OID("1.3.6.1.2.1"));
+        assertEquals(responses.size(), 18);
 
-        responsePDU = getResponse(new OID("1.3.6.1.2.1.31"));
-        assertEquals(responsePDU.getVariableBindings().size(), 11); // 10 + endOfMibView
+        responses = getResponse(new OID("1.3.6.1.2.1.31"));
+        assertEquals(responses.size(), 10);
 
-        responsePDU = getResponse(new OID(".1.3.6.1.2.1.2"));
-        assertEquals(responsePDU.getVariableBindings().size(), 18); // 17 + endOfMibView
+        responses = getResponse(new OID(".1.3.6.1.2.1.2"));
+        assertEquals(responses.size(), 7);
     }
-
+    
     @AfterMethod
     public void stopSnmpman() throws Exception {
         snmpman.stop();
     }
 
-    private static PDU getResponse(final OID query) throws Exception {
+    private static List<TableEvent> getResponse(final OID query) throws Exception {
         final Address targetAddress = GenericAddress.parse("127.0.0.1/10000");
         final TransportMapping transport = new DefaultUdpTransportMapping();
         final Snmp snmp = new Snmp(transport);
@@ -51,16 +55,11 @@ public class SnmpmanTest {
         target.setRetries(2);
         target.setTimeout(1500);
         target.setVersion(SnmpConstants.version2c);
-        // creating PDU
-
-        final PDU pdu = new PDU();
-        pdu.add(new VariableBinding(query));
-        pdu.setType(PDU.GETBULK);
-        pdu.setMaxRepetitions(Short.MAX_VALUE);
-        pdu.setNonRepeaters(0);
-        // sending request
         
-        final ResponseEvent responseEvent = snmp.send(pdu, target, transport);
-        return responseEvent.getResponse();
+        // creating PDU
+        final PDUFactory pduFactory = new DefaultPDUFactory(PDU.GETBULK);
+        final TableUtils utils = new TableUtils(snmp, pduFactory);
+
+        return utils.getTable(target, new OID[]{ query }, null, null);
     }
 }
