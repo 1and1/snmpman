149b4fdc9351a4c8fd8f418c0a1597e8f35fc3b0
Stephan Fuhrmann <s@sfuhrm.de>
2019-04-08 19:02:18 +0200

    Move fields to class begin

--- snmpman/src/main/java/com/oneandone/snmpman/configuration/AgentConfiguration.java
+++ snmpman/src/main/java/com/oneandone/snmpman/configuration/AgentConfiguration.java
@@ -28,49 +28,6 @@ public class AgentConfiguration {
     private static final DeviceFactory DEVICE_FACTORY = new DeviceFactory();
 
     /**
-     * The device factory creates all {@link Device} representations.
-     * <br>
-     * It is required to instantiate the representations only one time application-wide.
-     */
-    // TODO each Snmpman instance should have its own device factory instance
-    public static class DeviceFactory {
-
-        /** The default device which will be returned if no configuration specified. */
-        public static final Device DEFAULT_DEVICE = new Device("default", new Modifier[0], null);
-
-        /** The map of all available devices. */
-        private final Map<File, Device> devices = new HashMap<>();
-
-        /**
-         * Returns the device representation for the specified {@code path}.
-         * <br>
-         * If the {@code path} is {@code null}, or the parsing of the configuration failed, the {@link #DEFAULT_DEVICE} will be returned instead.
-         *
-         * @param path the path of the device configuration
-         * @return the {@link Device} representation for the specified configuration in {@code path}
-         */
-        public Device getDevice(final File path) {
-            if (path == null) {
-                return DEFAULT_DEVICE;
-            }
-
-            if (devices.containsKey(path)) {
-                return devices.get(path);
-            } else {
-                try {
-                    final ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-                    final Device device = mapper.readValue(path, Device.class);
-                    devices.put(path, device);
-                    return device;
-                } catch (final IOException e) {
-                    log.error("could not load device in path \"" + path.getAbsolutePath() + "\"", e);
-                    return DEFAULT_DEVICE;
-                }
-            }
-        }
-    }
-
-    /**
      * Returns the name of the agent.
      * <br>
      * If the {@code name} wasn't set on construction, the name will be defined by the {@link #address}.
@@ -113,6 +70,49 @@ public class AgentConfiguration {
      * @return the community for the agent
      */
     @Getter private final String community; // e.g. 'public'
+
+    /**
+     * The device factory creates all {@link Device} representations.
+     * <br>
+     * It is required to instantiate the representations only one time application-wide.
+     */
+    // TODO each Snmpman instance should have its own device factory instance
+    public static class DeviceFactory {
+
+        /** The default device which will be returned if no configuration specified. */
+        public static final Device DEFAULT_DEVICE = new Device("default", new Modifier[0], null);
+
+        /** The map of all available devices. */
+        private final Map<File, Device> devices = new HashMap<>();
+
+        /**
+         * Returns the device representation for the specified {@code path}.
+         * <br>
+         * If the {@code path} is {@code null}, or the parsing of the configuration failed, the {@link #DEFAULT_DEVICE} will be returned instead.
+         *
+         * @param path the path of the device configuration
+         * @return the {@link Device} representation for the specified configuration in {@code path}
+         */
+        public Device getDevice(final File path) {
+            if (path == null) {
+                return DEFAULT_DEVICE;
+            }
+
+            if (devices.containsKey(path)) {
+                return devices.get(path);
+            } else {
+                try {
+                    final ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+                    final Device device = mapper.readValue(path, Device.class);
+                    devices.put(path, device);
+                    return device;
+                } catch (final IOException e) {
+                    log.error("could not load device in path \"" + path.getAbsolutePath() + "\"", e);
+                    return DEFAULT_DEVICE;
+                }
+            }
+        }
+    }
 
     /**
      * Constructs a new agent configuration.
