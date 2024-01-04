247d8934676b4cdb082a8bfa569f1f7a1705d958
Stephan Fuhrmann <stephan.fuhrmann@ionos.com>
2024-01-04 12:42:51 +0100

    Make SNMPMAN_PORT, SNMPMAN_COMMUNITY and SNMPMAN_WALK configurable

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 set -e
 
+if [[ "$SNMPMAN_PORT" = "" ]]; then
+  export SNMPMAN_PORT="10000"
+fi
+
+if [[ "$SNMPMAN_COMMUNITY" = "" ]]; then
+  export SNMPMAN_COMMUNITY="public"
+fi
+
+if [[ "$SNMPMAN_WALK" = "" ]]; then
+  export SNMPMAN_WALK="/snmpman/etc/walk/example1.walk"
+fi
+
 if [[ "$SNMPMAN_CONFIG" = "" ]]; then
     export SNMPMAN_CONFIG=/snmpman/etc/configuration.yaml
-    echo "Creating dummy config in $SNMPMAN_CONFIG UDP port 10000, override with env var SNMPMAN_CONFIG"
+    echo "Creating dummy config in $SNMPMAN_CONFIG UDP port $SNMPMAN_PORT, override with env var SNMPMAN_CONFIG"
     cat >> $SNMPMAN_CONFIG << CONFIG
 - name: "example1"
   device: "/snmpman/etc/devices/ios.yaml"
-  walk: "/snmpman/etc/walk/example1.walk"
+  walk: "$SNMPMAN_WALK"
   ip: "0.0.0.0"
-  port: 10000
-  community: public
+  port: $SNMPMAN_PORT
+  community: $SNMPMAN_COMMUNITY
 CONFIG
 fi
 
