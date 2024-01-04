76655c460049d40c30bc3b99742d0c51d183d0b2
Stephan Fuhrmann <stephan.fuhrmann@ionos.com>
2024-01-04 15:26:40 +0100

    Allow log4j config file passing using LOGXML (snmpman.sh) Rewrite the maven project version in the caller script snmpman.sh

--- snmpman-cli/src/main/assembly/snmpman.sh
+++ snmpman-cli/src/main/assembly/snmpman.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
 BASEDIR=$(cd $(dirname "$0")/..; pwd)
-JAR=${BASEDIR}/lib/snmpman-cli-*.jar
+JAR=${BASEDIR}/lib/snmpman-cli-${project.version}.jar
+if [ "$LOGXML" = "" ]; then
+  export LOGXML=file:///${BASEDIR}/etc/log4j2.xml
+fi
 
-java -Dlog4j.configurationFile=file:///${BASEDIR}/etc/log4j2.xml -jar ${JAR} "$@"
+java -Dlog4j.configurationFile=${LOGXML} -jar ${JAR} "$@"
